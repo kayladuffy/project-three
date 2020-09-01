8024b7dab83775ffe31dc8ac4fb451090b718580
Kayla.duffy <kayla.duffy@whag18kayla.lan>
2020-09-01 12:00:03 -0500

    added props to link projects to respective URL

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   return(
       <nav className="nav-wrapper grey darken-3">
           <div className="container">
-            <Link to='/' className="brand-logo">React Task Manager</Link>
+            <Link to='/' className="brand-logo">React Project Manager</Link>
             <SignedInLinks />
             <SignedOutLinks />
           </div>
---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ProjectSummary from './ProjectSummary'
+
+const ProjectDetails = (props) => {
+    const id = props.match.params.id;
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+    <span className="card-title">Project Title - {id}</span>
+                    <p>Lorem Ipsum Text</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>Posted by Kayla Duffy</div>
+                    <div>Sep 1, 10am</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails
