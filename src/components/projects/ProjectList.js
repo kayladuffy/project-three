ed8d5a085dfa9bac64be37f48af4ec7cd73f8c22
Kayla.duffy <kayla.duffy@whag18kayla.lan>
2020-09-01 21:37:49 -0500

    Used redux to grab console data to display on web

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -5,7 +5,9 @@ const ProjectList = ({projects}) => {
     return(
         <div className="project-list section">
            { projects && projects.map(project => {
-               
+             return (
+                 <ProjectSummary project={project} key={project.id} />
+             )  
            })}
         </div>
 
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import projectReducer from '../../store/reducers/projectReducer';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
+                <span className="card-title">{project.title}</span>
                 <p>Posted by Kayla Duffy</p>
                 <p className="grey-text">Sep 1, 10am</p>
             </div>
